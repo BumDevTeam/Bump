199a249384cc0033380fba20d74009175d6b5ce0
yumitaru <jakubzajac0222@gmail.com>
2024-06-25 19:46:40 +0200

    App working with server 1.0
    Co-authored-by: michalshy <91937056+michalshy@users.noreply.github.com>
    
    Co-authored-by: Saesenthessis66 <lucjangruby@gmail.com>

--- app/src/main/java/com/example/bump/Controller/APIController/Api.kt
+++ app/src/main/java/com/example/bump/Controller/APIController/Api.kt
@@ -1,5 +1,14 @@
 package com.example.bump.Controller.APIController
 
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.fragment.app.FragmentActivity
+import com.google.android.gms.maps.model.LatLng
+import com.google.gson.GsonBuilder
+import com.google.gson.JsonDeserializer
+import com.google.gson.reflect.TypeToken
 import okhttp3.Call
 import okhttp3.Callback
 import okhttp3.MediaType.Companion.toMediaTypeOrNull
@@ -9,30 +18,20 @@ import okhttp3.RequestBody.Companion.toRequestBody
 import okhttp3.Response
 import okio.IOException
 
-
-//          ____________usage______________
-
-//val api = Api()
-//
-//        api.getAll { response ->
-//            Log.i("NUHUH!", response)
-//        }
-//        api.addEntry("4.87167","4.654014","6.66")
-//        {
-//                response ->  Log.i("YEAH!", response)
-//        }
-//        api.getNearby("5.87167","5.654014")
-//        {
-//            response ->  Log.i("MICHAL!", response)
-//        }
-
-
 class Api {
 
     private val client = OkHttpClient()
+    val gson = GsonBuilder()
+        .registerTypeAdapter(LatLng::class.java, JsonDeserializer { json, _, _ ->
+            val jsonObject = json.asJsonObject
+            LatLng(
+                jsonObject.get("latitude").asDouble,
+                jsonObject.get("longitude").asDouble
+            )
+        })
+        .create()
 
     fun getAll(callback: (String) -> Unit) {
-        var ret = ""
         val request = Request.Builder()
             .url("https://bumpserver.fly.dev/api/entry")
             .build()
@@ -46,22 +45,19 @@ class Api {
             override fun onResponse(call: Call, response: Response)   {
                 response.use {
                     if (!response.isSuccessful) throw IOException("Unexpected code $response")
-                    ret =  response.body!!.string()
-                    callback(ret)
+                    callback(response.body!!.string())
                 }
             }
         })
     }
 
-    fun getNearby(longitude: String, latitude:String, callback: (String) -> Unit)
-    {
-        var ret = ""
+    fun getNearby(longitude: String, latitude: String, callback: (String) -> Unit) {
         val json = """
         {
             "longitude": "$longitude",
             "latitude": "$latitude"
         }
-         """.trimIndent()
+        """.trimIndent()
 
         val requestBody = json.toRequestBody("application/json".toMediaTypeOrNull())
 
@@ -73,33 +69,28 @@ class Api {
         client.newCall(request).enqueue(object : Callback {
             override fun onFailure(call: Call, e: IOException) {
                 e.printStackTrace()
-                // Handle failure
             }
 
             override fun onResponse(call: Call, response: Response) {
                 response.use {
-                    if (!response.isSuccessful) {
-                        // Handle unsuccessful response - there will be not unsuccessful response
-                        callback("")
+                    if (response.isSuccessful) {
+                        callback(response.body!!.string())
                     } else {
-                        // Handle successful response
-                        ret =  response.body!!.string()
-                        callback(ret)
+                        callback("")
                     }
                 }
             }
         })
     }
-    fun addEntry(longitude: String, latitude:String,value:String, callback: (String) -> Unit)
-    {
-        var ret = ""
+
+    fun addEntry(longitude: String, latitude: String, value: String, callback: (String) -> Unit) {
         val json = """
         {
             "longitude": "$longitude",
             "latitude": "$latitude",
             "value": "$value"
         }
-    """.trimIndent()
+        """.trimIndent()
 
         val requestBody = json.toRequestBody("application/json".toMediaTypeOrNull())
 
@@ -111,21 +102,36 @@ class Api {
         client.newCall(request).enqueue(object : Callback {
             override fun onFailure(call: Call, e: IOException) {
                 e.printStackTrace()
-                // Handle failure - only failure here is you
             }
 
             override fun onResponse(call: Call, response: Response) {
                 response.use {
-                    if (!response.isSuccessful) {
-                        // Handle unsuccessful response - there will be not unsuccessful response
-                        callback("")
+                    if (response.isSuccessful) {
+                        callback(response.body!!.string())
                     } else {
-                        // Handle successful response
-                        ret =  response.body!!.string()
-                        callback(ret)
+                        callback("")
                     }
                 }
             }
         })
+    }
+
+    fun returnParsed(x: Double, y: Double, list: MutableState<List<LatLng?>>) {
+        this.getNearby(x.toString(), y.toString()) { response ->
+            if (response.isNotEmpty()) {
+                // Log the response to ensure it contains expected data
+                Log.d("ApiResponse", "Response: $response")
+
+                // Parse the response into a List of LatLng
+                val latLngListType = object : TypeToken<List<LatLng?>>() {}.type
+                val parsedList: List<LatLng?> = gson.fromJson(response, latLngListType)
+
+                // Log the parsed list to verify contents
+                Log.d("ParsedList", "Parsed List: $parsedList")
+
+                // Update the mutable state with the parsed list
+                list.value = parsedList
+            }
+        }
     }
 }
